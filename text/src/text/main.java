8ef7b4e0b017b105cea6af05d56a1ebf2db7ca85
俊麟 <@waybq>
2017-10-08 21:19:53 +0800

    5

---
+++ text/src/text/main.java
@@ -0,0 +1,13 @@
+package text;
+
+public class main {
+	public static void main(String[] args){
+	text l=new text();
+	System.out.println(l.i);
+	//System.out.println(l.j);
+	System.out.println(l.k);
+	l.method1();
+	//l.method2();
+	l.method3();
+	}
+}
---
+++ text/src/text/text.java
@@ -0,0 +1,14 @@
+package text;
+
+public class text 
+{
+	public int i;
+	private int j;
+	protected int k;
+	public void method1()
+	{System.out.println("public method");}
+	private void method2()
+	{System.out.println("private method");}
+	protected void method3()
+	{System.out.println("protected method");}
+}
